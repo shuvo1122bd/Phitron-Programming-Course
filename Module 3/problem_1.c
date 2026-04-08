296e5e6e196fb92f74c76fb1d5402e803f75702f
shuvo <shuvovlog223@gmail.com>
2026-04-08 12:44:52 +0600

    practiced loop problems

---
+++ Module 3/problem_1.c
@@ -0,0 +1,23 @@
+// #include<stdio.h>
+// int main ()
+// {
+//     for (int i = 8; i <= 200; i += 8)
+//     {
+//         printf("%d\n",i);
+//     }
+//     return 0;
+// }
+
+// #include<stdio.h>
+// int main ()
+// {
+//     for (int i = 1; i <= 10; i++)
+//     {
+//         int multi = i * 8;
+//         {
+//             printf("%d\n",multi);
+//         }
+        
+//     }
+//     return 0;
+// }

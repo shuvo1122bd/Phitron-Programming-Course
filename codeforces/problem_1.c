261a6d94cac6acc8e2801950a7dcdeb91edd9b6e
shuvo <shuvovlog223@gmail.com>
2026-04-10 21:01:42 +0600

    solved ocdeforces first problem

--- codeforces/problem_1.c
+++ codeforces/problem_1.c
@@ -4,8 +4,8 @@ int main()
     int w;
     scanf("%d", &w);
     if (w >= 1 &&  w <=100)
-    {
-        if(w%2 == 0){
+    {  
+        if(w%2 == 0 && w != 2){
             printf("YES");
         } else {
             printf("NO");

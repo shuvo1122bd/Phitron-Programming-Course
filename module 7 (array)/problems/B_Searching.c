a3b51a4843bdab714904a7fe4b9ce3f541cd6960
shuvo <shuvovlog223@gmail.com>
2026-04-17 22:00:54 +0600

    comment

--- module 7 (array)/problems/B_Searching.c
+++ module 7 (array)/problems/B_Searching.c
@@ -2,18 +2,19 @@
 int main()
 {
     int n;
-    scanf("%d", &n);
-    long long int ar[n];
+    scanf("%d", &n); //input N
+
+    long long int ar[n]; //input array with loop
     for (int i = 0; i < n; i++)
     {
         scanf("%lld", &ar[i]);
     }
 
-    long long int x;
+    long long int x; //input X 
     scanf("%lld", &x);
 
-    int u = 0;
-    int value = 0;
+    int u = 0; //input i from loop
+    int value = 0; //input i value = ar[i] from loop
 
     for (int i = 0; i < n; i++)
     {
@@ -26,13 +27,13 @@ int main()
         }
     }
 
-    if (value == x)
+    if (value == x) // value = ar[i] that should be equal to x
     {
         printf("%d", u);
     }
     else
     {
-        printf("-1");
+        printf("-1"); //if no condition will be met.
     }
 
     return 0;

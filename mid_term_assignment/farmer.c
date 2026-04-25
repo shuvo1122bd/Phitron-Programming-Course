d441ed1be7fc309b5419d4ce7f4c509674108e39
shuvo <shuvovlog223@gmail.com>
2026-04-25 16:51:18 +0600

    solving array releated problems

--- mid_term_assignment/farmer.c
+++ mid_term_assignment/farmer.c
@@ -6,14 +6,13 @@ int main()
     for(int i = 0; i < t; i++)
     {
         int m1, m2, d;
-        scanf("%d %d %d", &m1,&m2,&d);
+        scanf("%d %d %d", &m1, &m2, &d);
         int with_additional_farmer = m1+m2; //total farmer with additional farmer
         int total_days = m1*d/with_additional_farmer;
 
         int fewer_days = d - total_days;
 
         printf("%d\n", fewer_days);
-
     }
     return 0;
 }

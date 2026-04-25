2fbd08dea3ca79600efa6bf22318b02218b02400
shuvo <shuvovlog223@gmail.com>
2026-04-25 18:42:46 +0600

    new

--- E_Lowest_Number.c
+++ E_Lowest_Number.c
@@ -1,6 +1,31 @@
 #include<stdio.h>
 int main()
 {
-    
+    int n;
+    scanf("%d", &n);
+    int a[n];
+    for(int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    int lowest_number = a[0];
+    int position = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] < lowest_number)
+        {
+            lowest_number = a[i];
+            position = i + 1;
+        }
+        else if(i == 0 && a[i] == lowest_number) // if index number == 0; that is solution;
+        {
+            position = i + 1;
+        }
+        
+    }
+
+    printf("%d %d", lowest_number, position);
     return 0;
 }

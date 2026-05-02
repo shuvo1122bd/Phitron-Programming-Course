4078111c73f239619b16a15853526b21de5e19fa
shuvo <shuvovlog223@gmail.com>
2026-05-03 00:48:01 +0600

    learning pattern

---
+++ Module 13/pyramid.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    int star = 1;
+    int space = n-1;
+
+    for(int i = 1; i<=n;i++) //printing new lines
+    {
+        for(int j = 1; j <= space; j++)
+        {
+            printf(" ");
+        }
+        for(int  j = 1; j <= star; j++) //printing stars
+        {
+            printf("*");
+        }
+        printf("\n");
+        star += 2;
+        space--;
+    }
+
+    return 0;
+}

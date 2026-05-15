a8fe11edf8512516eb485d566127cdc542df8eb3
nishuvo <110178244+shuvo1122bd@users.noreply.github.com>
2026-05-15 20:03:51 +0600

    final assignment done, and completed my C language course, Alhamdulillah

---
+++ Final assignment/Difference_Array.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        int a[n];
+        for (int i = 0; i < n; i++)
+        {
+            scanf("%d", &a[i]);
+        }
+
+        int b[n];
+        for (int i = 0; i < n; i++)
+        {
+            b[i] = a[i];
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (b[i] > b[j])
+                {
+                    int temp = b[i];
+                    b[i] = b[j];
+                    b[j] = temp;
+                }
+            }
+        }
+
+        int c[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            int diff = abs(a[i] - b[i]);
+            c[i] = diff;
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            printf("%d ", c[i]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
